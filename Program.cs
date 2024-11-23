01b7a34a940bc94edf7bdd301ecdeacc7f092474
Tufteds <prosional23457008@gmail.com>
2024-11-23 18:26:37 +0900

    Практика Урок2 abc

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string name = Console.ReadLine();
+Console.WriteLine("Привет, " + name);
+
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine(2024 - age);
+
+//
+
+string name1 = Console.ReadLine();
+int phone = int.Parse(Console.ReadLine());
+Console.WriteLine("Контакт сохранён." + '\n' + name1 + " - " + phone);
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+//
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine((num / 100) % 10);
+Console.WriteLine((num / 10) % 10);
+Console.WriteLine(num % 10);
